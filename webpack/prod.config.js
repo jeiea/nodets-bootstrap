f5e645d1376083034c8e4f9b1d2ad1c0ebd62766
jeiea <solum5013@gmail.com>
2019-04-29 01:10:30 +0900

    Apply TerserPlugin minifier

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,21 +1,31 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/app.ts'),
   mode: 'production',
   module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        include: path.resolve(__dirname, '../src'),
-        exclude: /node_modules/
-      }
-    ]
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      include: path.resolve(__dirname, '../src'),
+      exclude: /node_modules/
+    }]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
   target: 'node',
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'dist/index.js',
